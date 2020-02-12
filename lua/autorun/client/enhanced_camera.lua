33816dd320302e742f2b7ea81e7f8a314fbf088f
VictorienXP <VictorienXP@Xperidia.com>
2020-02-12 19:02:17 +0100

    Add a check for valid bones (prevent Lua errors)

--- lua/autorun/client/enhanced_camera.lua
+++ lua/autorun/client/enhanced_camera.lua
@@ -303,7 +303,9 @@ function EnhancedCamera:OnPoseChange()
   local wep = LocalPlayer():GetActiveWeapon()
   local name = IsValid(wep) and wep:GetClass() or ""
   local bone = self.entity:LookupBone("ValveBiped.Bip01_Head1")
-  self.entity:ManipulateBoneScale(bone, vector_origin)
+  if bone then
+    self.entity:ManipulateBoneScale(bone, vector_origin)
+  end
   if not cvarHair:GetBool() then
     self.entity:ManipulateBonePosition(bone, Vector(-128, 128, 0))
   end
@@ -312,26 +314,34 @@ function EnhancedCamera:OnPoseChange()
        NAME_SHOW_ARM.left[name]) and not
        NAME_HIDE_ARM.left[name]) then
     bone = self.entity:LookupBone("ValveBiped.Bip01_L_Upperarm")
-    self.entity:ManipulateBoneScale(bone, vector_origin)
-    self.entity:ManipulateBonePosition(bone, Vector(0, 0, -128))
+    if bone then
+       self.entity:ManipulateBoneScale(bone, vector_origin)
+       self.entity:ManipulateBonePosition(bone, Vector(0, 0, -128))
+    end
   end
   if self.apiBoneHide['r_arm'] or self.reloading or not (
       (POSE_SHOW_ARM.right[self.pose] or
        NAME_SHOW_ARM.right[name]) and not
        NAME_HIDE_ARM.right[name]) then
     bone = self.entity:LookupBone("ValveBiped.Bip01_R_Upperarm")
-    self.entity:ManipulateBoneScale(bone, vector_origin)
-    self.entity:ManipulateBonePosition(bone, Vector(0, 0, 128))
+    if bone then
+       self.entity:ManipulateBoneScale(bone, vector_origin)
+       self.entity:ManipulateBonePosition(bone, Vector(0, 0, 128))
+    end
   end
   if self.apiBoneHide['l_leg'] then
     bone = self.entity:LookupBone("ValveBiped.Bip01_L_Thigh")
-    self.entity:ManipulateBoneScale(bone, vector_origin)
-    self.entity:ManipulateBonePosition(bone, Vector(0, 0, -128))
+    if bone then
+       self.entity:ManipulateBoneScale(bone, vector_origin)
+       self.entity:ManipulateBonePosition(bone, Vector(0, 0, -128))
+    end
   end
   if self.apiBoneHide['r_leg'] then
     bone = self.entity:LookupBone("ValveBiped.Bip01_R_Thigh")
-    self.entity:ManipulateBoneScale(bone, vector_origin)
-    self.entity:ManipulateBonePosition(bone, Vector(0, 0, -128))
+    if bone then
+       self.entity:ManipulateBoneScale(bone, vector_origin)
+       self.entity:ManipulateBonePosition(bone, Vector(0, 0, -128))
+    end
   end
 
   -- Set pose-specific view offset
