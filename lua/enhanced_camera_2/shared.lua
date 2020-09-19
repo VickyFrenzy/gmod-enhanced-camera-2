eaa33a3076f447d374341e1256047bd8fb0b07ea
VictorienXP <VictorienXP@Xperidia.com>
2020-09-19 21:26:51 +0200

    Add comment about the Mae PM fix

--- lua/enhanced_camera_2/shared.lua
+++ lua/enhanced_camera_2/shared.lua
@@ -1,5 +1,8 @@
 AddCSLuaFile()
 
 timer.Simple(0, function()
+
+    --Fix for https://steamcommunity.com/sharedfiles/filedetails/?id=1187366110
     hook.Remove("PlayerSpawn", "Mae_Viewheight_Offeset")
+
 end)
