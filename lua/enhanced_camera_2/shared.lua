13061e613f84d6e5651e5496edfb8ed3a21d4c33
VictorienXP <VictorienXP@Xperidia.com>
2020-11-23 01:05:24 +0100

    Update shared.lua

--- lua/enhanced_camera_2/shared.lua
+++ lua/enhanced_camera_2/shared.lua
@@ -2,7 +2,7 @@ AddCSLuaFile()
 
 timer.Simple(0, function()
 
-    --Fix for https://steamcommunity.com/sharedfiles/filedetails/?id=1187366110
-    hook.Remove("PlayerSpawn", "Mae_Viewheight_Offeset")
+	--Fix for https://steamcommunity.com/sharedfiles/filedetails/?id=1187366110
+	hook.Remove("PlayerSpawn", "Mae_Viewheight_Offeset")
 
 end)
